b2118d390497c07b78ee5ff0341273f25f316cab
Abdirabi440 <osmanabdirabi440@gmail.com>
2024-10-17 03:31:30 +0300

    complte lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBest) {
+  bestCustomer = newBest; 
+}
+
+const leastFavoriteCustomer = 'Yussuf'; 
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.'); // Throws an error if reassigning
+}
